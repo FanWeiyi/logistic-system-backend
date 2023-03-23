a67599c45f295f0a5fa9353ed06d497522de7e31
Carolyn <2535806990@qq.com>
2023-03-23 20:51:50 +0800

    first commit

---
+++ src/main/java/com/example/logisticsystembackend/LogisticSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.logisticsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/logisticsystembackend/LogisticSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logisticsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
